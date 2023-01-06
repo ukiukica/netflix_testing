1c87b63f385eda5702095df5ad4a022c9dc06d75
uki <ukiworks034@gmail.com>
2023-01-06 10:40:42 -0500

    initial commit / 21 tests added

---
+++ tests/netflix.spec.js
@@ -0,0 +1,84 @@
+const { test, expect } = require("@playwright/test");
+
+// run tests in headful mode so you can see the browser
+test.use({ headless: true, slowMo: 1000 });
+
+test("my first test", async ({ page }) => {
+  // go to Netflix.com
+  await page.goto("https://www.netflix.com");
+
+  // assert page title appears
+  await expect(page.locator('[data-uia="hero-title"]')).toHaveText(
+    "Unlimited movies, TV shows, and more."
+  );
+});
+
+// ADD YOUR TESTS HERE!
+
+// go to sign in page
+// try to sign in with invalid email/pass
+
+// -------------------------------------------
+
+test("sign in page exists", async({ page }) => {
+  await page.goto("https://www.netflix.com");
+
+  await page.getByRole('link', { name: /sign in/i}).click();
+
+  await expect(page).toHaveURL(/.*login/);
+  const response = await page.request.get(page.url());
+  await expect(response).toBeOK();
+
+  await expect(page.getByRole('heading', { name: /sign in/i})).toBeVisible(); // may not need this
+});
+
+// test initial confitions:
+
+// email or phone number field exists
+// -||- is empty
+test.describe("initial conditions", () => {
+  test.use({ baseURL: 'https://www.netflix.com'});
+
+  test("email input field is visible, editable, and empty", async ({ page }) => {
+    await page.goto("/");
+    await page.getByRole('link', { name: /sign in/i}).click();
+
+    const emailInputField = page.getByLabel(/email or phone number/i);
+    await expect(emailInputField).toBeVisible();
+    await expect(emailInputField).toBeEditable();
+    await expect(emailInputField).toBeEmpty();
+  });
+
+  test("password input field is visible, editable, and empty", async ({ page }) => {
+    await page.goto("/");
+    await page.getByRole('link', { name: /sign in/i}).click();
+
+    const passwordInputField = page.getByLabel(/password/i);
+    await expect(passwordInputField).toBeVisible();
+    await expect(passwordInputField).toBeEditable();
+    await expect(passwordInputField).toBeEmpty();
+  });
+
+  test('"Remember Me" checkbox is checked by default', async ({ page }) => {
+    await page.goto("/");
+    await page.getByRole('link', { name: /sign in/i}).click();
+
+    await expect(page.getByLabel(/remember me/i)).toBeChecked();
+  })
+})
+
+// password field exists
+// -||- is empty
+// when click "show" it shows the password
+
+// test("password input field is visible, editable, and empty", async )
+
+// checkbox "remember me" is unchecked
+
+// "need help" a tag exists
+// -||- takes you to the page that has "forgot email/password" text
+
+// "sign up now" a tag exists
+// -||- takes you to the page that has "sign up now" text
+
+// check language select field if exists and if works
